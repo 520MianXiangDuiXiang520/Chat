2781c33417db11187c1fb484ddc396c1ec24ff62
root <root@LAPTOP-EJ9LQIQ9.localdomain>
2020-06-24 10:13:33 +0800

    :wastebasket:clear

--- UserDB.h
+++ UserDB.h
@@ -13,12 +13,6 @@ using namespace std;
 class UserDB
 {
 private:
-	std::string UIDS[100] = { "0", "1", "2" };
-	std::string PSWS[100] = { "junebao","junebao2", "junebao3" };
-	std::string NAMES[100] = { "aJuneBao", "OOP", "Class" };
-	std::string ADDR[100] = { "127.0.0.1", "127.0.0.1", "127.0.0.1" };
-	bool ONLINE[100] = { false };
-	int INDEX = 3;
 	MYSQL *conn = mysql_init(NULL);
 public:
 	UserDB();
